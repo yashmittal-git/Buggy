ce78c59f7dd13ddce5571d3197ecd98b9088ca93
MrMittalY <40431640+MrMittalY@users.noreply.github.com>
2019-04-03 15:09:50 +0530

    Delete 3_brightness_led.ino

--- 3_brightness_led.ino
+++
@@ -1,1 +0,0 @@
-//control brightness of an LEDvoid setup() {  Serial.begin(9600);  pinMode(9,OUTPUT);}void loop() {  int brightness;  if(Serial.available())    {      brightness=Serial.read();      analogWrite(9,brightness);    }}
